4cb678deec1ec734ebd6aecb9b1ca77d3951df46
Shmedi <eduard.tupy@hotmail.com>
2023-07-31 00:32:05 -0400

    build: updated deps

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 import { store } from "./app/store"
 import { Provider } from "react-redux"
+import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 )
